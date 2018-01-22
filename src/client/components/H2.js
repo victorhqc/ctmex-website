439b4c3a473d1c32609288e9445f55d659343b8a
Victor Quiroz <victorhqc@gmail.com>
2018-01-22 21:17:37 +0100

    Forgot to add this title

--- src/client/components/H2.js
+++ src/client/components/H2.js
@@ -4,7 +4,7 @@ import { noMargin, setColor } from '../themes/utils';
 
 const H2 = styled.h2`
   font-family: ${getFontFamily('sourceSansPro')};
-  font-size: ${getFontSize('lg')}em;
+  font-size: ${getFontSize('xl')}em;
   font-weight: lighter;
 
   ${noMargin}
