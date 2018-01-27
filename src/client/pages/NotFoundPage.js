ad419a0cd2e8e7fe0ea22c1f39ac76e30dd3e59b
Victor Quiroz <victorhqc@gmail.com>
2018-01-27 13:32:40 +0100

    Improve a bit the 404 page.

--- src/client/pages/NotFoundPage.js
+++ src/client/pages/NotFoundPage.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
 import {
   NOT_FOUND,
 } from '../../constants';
+
+import WhiteTheme from '../themes/White';
+import Menu from '../components/Menu';
+import Container from '../components/Container';
 
 const NotFoundPage = ({ staticContext }) => {
   staticContext.set(NOT_FOUND, true);
 
   return (
-    <h1>Ups, página no encontrada.</h1>
+    <Fragment>
+      <Helmet>
+        <title>ctmex - 404</title>
+        <meta property="og:site_name" content="ctmex" />
+        <meta property="og:title" content="Colegio de tenis mexicano - 404" />
+        <meta property="og:url" content="http://colegiodetenis.mx/404" />
+        <meta property="og:type" content="website" />
+        <meta property="og:author" content="https://github.com/victorhqc" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:site" content="@colegiotenismx" />
+        <meta property="twitter:creator" content="https://github.com/victorhqc" />
+        <meta property="twitter:title" content="ctmex" />
+        <meta property="twitter:description" content="Colegio de tenis mexicano" />
+
+        <meta name="description" content="Colegio de tenis mexicano" />
+      </Helmet>
+      <WhiteTheme>
+        <Fragment>
+          <Menu />
+          <Container height="65vh" verticalAlign>
+            <h1>Ups, página no encontrada.</h1>
+          </Container>
+        </Fragment>
+      </WhiteTheme>
+    </Fragment>
   );
 };
 
