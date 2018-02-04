fad5815d60e64c488c57518dc33b864b95080957
Victor Quiroz <victorhqc@gmail.com>
2018-02-04 16:34:33 +0100

    Method page looking good!

--- src/client/pages/MethodPage.js
+++ src/client/pages/MethodPage.js
@@ -5,6 +5,11 @@ import map from 'lodash/map';
 
 import ThemeProvider from '../themes/Provider';
 
+import {
+  getColor,
+  getMargin,
+} from '../themes/base';
+
 import Menu from '../components/Menu';
 import Container from '../components/Container';
 import Row from '../components/Row';
@@ -13,13 +18,30 @@ import Background from '../components/Background';
 import H2 from '../components/H2';
 import P from '../components/P';
 import Logo from '../components/Logo';
+import InfoContainer from '../components/InfoContainer';
 
 import methodCover from '../../assets/pictures/method_cover.jpg';
+import yellowTennis from '../../assets/yellow_tennis.svg';
 
 import {
   INTRO_TITLE,
   INTRODUCTION,
 } from '../../locales/es/method';
+
+const TennisDoodle = styled.div`
+  background-image: url(${yellowTennis});
+  background-color: ${getColor('seaBuckthorn')};
+  background-size: auto 100%;
+  background-repeat: no-repeat;
+
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
+const Box = styled.div`
+  margin-top: ${getMargin('md')};
+`;
 
 const MethodPage = () => (
   <Fragment>
@@ -42,20 +64,31 @@ const MethodPage = () => (
     <ThemeProvider color="black">
       <Menu />
     </ThemeProvider>
-    <Background src={methodCover} height="60vh">
+    <Background
+      src={methodCover}
+      height="60vh"
+      backgroundPosition="0 65%"
+    >
       <Logo />
     </Background>
-    <ThemeProvider color="white">
-      <Container>
-        <H2>{INTRO_TITLE}</H2>
-        <Row>
-          <Column size={2 / 3}>
-            {map(INTRODUCTION, (value, key) => (
-              <P lead key={key}>{value}</P>
-            ))}
-          </Column>
-        </Row>
-      </Container>
+    <ThemeProvider color="yellow">
+      <Box>
+        <Container noMargin noPadding>
+          <Row>
+            <Column size={2 / 5}>
+              <TennisDoodle />
+            </Column>
+            <Column size={3 / 5}>
+              <InfoContainer marginLeft={80} width="80%">
+                <H2>{INTRO_TITLE}</H2>
+                {map(INTRODUCTION, (value, key) => (
+                  <P lead key={key}>{value}</P>
+                ))}
+              </InfoContainer>
+            </Column>
+          </Row>
+        </Container>
+      </Box>
     </ThemeProvider>
   </Fragment>
 );
