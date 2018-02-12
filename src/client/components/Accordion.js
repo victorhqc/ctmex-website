fd5c29df22e584af6ab77bb7fad22257b73cda6d
Victor Quiroz <victorhqc@gmail.com>
2018-02-12 21:21:34 +0100

    Remove unused Fragment

--- src/client/components/Accordion.js
+++ src/client/components/Accordion.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import map from 'lodash/map';
